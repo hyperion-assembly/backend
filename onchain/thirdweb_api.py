b4bc95dd1319a7195632fffe62d8bf02ae472bf6
hellno <hellno@users.noreply.github.com>
2023-12-16 16:20:53 +0100

    update thirdweb-sdk package

--- onchain/thirdweb_api.py
+++ onchain/thirdweb_api.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-from typing import List, Optional
 from thirdweb import ThirdwebSDK
 from thirdweb.types import SDKOptions
 from thirdweb.types.settings.metadata import TokenContractMetadata
